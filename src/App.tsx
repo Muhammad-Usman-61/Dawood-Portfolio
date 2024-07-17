bc36b3868b9c2b14a3b55e283653c0926f146c03
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-07-17 20:15:15 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-red-400 text-9xl w-full h-screen flex items-center justify-center">
+      <div>Dawood 🤌</div>
+    </div>
+  );
+}
+
+export default App;
